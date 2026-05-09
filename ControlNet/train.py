5736ffb205a1ef106e858cd3f8e67e06d11ce201
kev556 <26943287+kev556@users.noreply.github.com>
2026-05-09 00:55:57 +0000

    added functionality to save ckpt per epoch

--- ControlNet/train.py
+++ ControlNet/train.py
@@ -4,26 +4,24 @@ import torch
 import pytorch_lightning as pl
 import json
 from torch.utils.data import DataLoader, random_split
-from ControlNet.dataset import MyDataset
+from dataset import MyDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint
 
-# Suppress intentional PyTorch DDP stream mismatch warnings
 import torch.autograd.graph
 torch.autograd.graph.set_warn_on_accumulate_grad_stream_mismatch(False)
 
 # Configs
 resume_path = './models/control_sd15_canny.pth'
-batch_size = 32
+batch_size = 64
 logger_freq = 300
 learning_rate = 1e-5
 sd_locked = True
 only_mid_control = False
 
 
-# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
 model = create_model('./models/cldm_v15.yaml').cpu()
 model.load_state_dict(load_state_dict(resume_path, location='cpu'), strict=False)
 model.learning_rate = learning_rate
@@ -38,10 +36,12 @@ checkpoint_callback = ModelCheckpoint(
     dirpath="./models/",
     filename="control_sd15_canny_finetuned-{epoch:02d}-{step}",
     save_top_k=2,
-    save_last=True
+    save_last=True,
+    monitor="val_loss",
+    mode="min",
 )
 
-trainer = pl.Trainer(accelerator='gpu', devices=1, strategy='ddp_find_unused_parameters_true', precision=16, callbacks=[logger, TQDMProgressBar(), checkpoint_callback], max_epochs=50)
+trainer = pl.Trainer(accelerator='gpu', devices=4, strategy='ddp_find_unused_parameters_true', precision="16-mixed", callbacks=[logger, TQDMProgressBar(), checkpoint_callback], max_epochs=50)
 
 full_dataset = MyDataset()
 
@@ -50,8 +50,8 @@ val_size = len(full_dataset) - train_size
 
 train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
 
-train_dataloader = DataLoader(train_dataset, num_workers=8, batch_size=32, shuffle=True)
-val_dataloader = DataLoader(val_dataset, num_workers=8, batch_size=32, shuffle=False, persistent_workers=True)
+train_dataloader = DataLoader(train_dataset, num_workers=8, batch_size=batch_size, shuffle=True)
+val_dataloader = DataLoader(val_dataset, num_workers=8, batch_size=batch_size, shuffle=False, persistent_workers=True)
 
 val_indices = val_dataset.indices
 val_data_items = [full_dataset.data[idx] for idx in val_indices]
